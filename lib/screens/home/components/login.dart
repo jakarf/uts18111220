5f956cb353258fed4448152f88d264140d7721ae
unknown <jakarizkifahriza@gmail.com>
2020-12-11 20:54:19 +0700

    utsmobile

---
+++ lib/screens/home/components/login.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/screens/home/components/register.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:shop_app/screens/home/components/body.dart';
+import 'package:shop_app/screens/home/components/login.dart';
+import 'package:shop_app/screens/home/components/about.dart';
+
+class loginpage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ColorPalette.primaryColor,
+        padding: EdgeInsets.all(30.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _iconLogin(),
+                  _titleDescription(),
+                  _textField(),
+                  _buildButton(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget _iconLogin() {
+  return Image.asset(
+    "assets/images/logo.png",
+    height: 150.0,
+    width: 150.0,
+  );
+}
+
+Widget _titleDescription() {
+  return Column(
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.only(top: 30.0),
+      ),
+      Text(
+        "Login Account",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(top: 20.0),
+      ),
+      Text(
+        "Apple - Think Different",
+        style: TextStyle(
+          fontSize: 12.0,
+          color: Colors.white,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ],
+  );
+}
+
+Widget _textField() {
+  return Column(
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.only(top: 60.0),
+      ),
+      TextFormField(
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: ColorPalette.underlineTextField,
+              width: 1.5,
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 3.0,
+            ),
+          ),
+          hintText: "Username",
+          hintStyle: TextStyle(color: ColorPalette.hintColor),
+        ),
+        style: TextStyle(color: Colors.white),
+        autofocus: false,
+      ),
+      Padding(
+        padding: EdgeInsets.only(top: 12.0),
+      ),
+      TextFormField(
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: ColorPalette.underlineTextField,
+              width: 1.5,
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 3.0,
+            ),
+          ),
+          hintText: "Password",
+          hintStyle: TextStyle(color: ColorPalette.hintColor),
+        ),
+        style: TextStyle(color: Colors.white),
+        obscureText: true,
+        autofocus: false,
+      ),
+    ],
+  );
+}
+
+Widget _buildButton(context) {
+  return Column(
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.only(top: 16.0),
+      ),
+      InkWell(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          width: double.infinity,
+          child: Text(
+            'Login',
+            style: TextStyle(color: ColorPalette.primaryColor),
+            textAlign: TextAlign.center,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(top: 16.0),
+      ),
+      Text(
+        'or',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 12.0,
+        ),
+      ),
+      FlatButton(
+        child: Text(
+          'Register',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => RegisterPage()));
+        },
+      ),
+    ],
+  );
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/screens/home/components/body.dart';
+import 'package:shop_app/screens/home/components/login.dart';
+import 'package:shop_app/screens/home/components/about.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            // By default our  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            // By default our  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/account.svg",
+            // By default our  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => loginpage()));
+          },
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/about.svg",
+            // By default our  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => about()));
+          },
+        ),
+        SizedBox(width: kDefaultPaddin / 2)
+      ],
+    );
+  }
+}
